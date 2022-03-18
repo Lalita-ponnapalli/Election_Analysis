7e3c18143ec3f9b0e624ed8c81f8834f2a7b4ff7
lalita-ponnapalli <lalita.pn1@gmail.com>
2022-03-18 10:25:33 -0500

    pypoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,13 @@ file_to_load = os.path.join("resources","election_results.csv")
 file_to_save = os.path.join("Analysis","Election_analysis")
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-    for row in file_reader:
-        print(row)    
+    # Print the header row.
+    headers = next(file_reader)
+    print(headers)
 
+
+#   file_reader = csv.reader(election_data)
+ #   for row in file_reader:
+  #    print(row)    
+
+# Read the file object with the reader function.
